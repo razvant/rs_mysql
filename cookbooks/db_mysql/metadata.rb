302c0250cc4e74f2408364c9dcfe410be3350a57
Razvan <razvant7@gmail.com>
2013-02-26 22:27:21 +0200

    Metadata

--- cookbooks/db_mysql/metadata.rb
+++ cookbooks/db_mysql/metadata.rb
@@ -2,8 +2,8 @@ maintainer       "Stratalux, Inc."
 maintainer_email "razvan.tomescu@stratalux.com"
 license          "Copyright Stratalux, Inc. All rights reserved."
 description      "Installs/configures a MySQL database client and server."
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "12.1.0"
+long_description "Installs/configures a MySQL database client and server."
+version          "1.0.0"
 
 
 
